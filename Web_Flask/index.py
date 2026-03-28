986474e32c22e881fba36de7ed5ccb64c48bfdc3
SHALAWA <bambukvova3@gmail.com>
2026-03-28 12:40:07 +0200

    Add zz function to script.py for debug from index.py script.py and add art (text2art) from style

--- Web_Flask/index.py
+++ Web_Flask/index.py
@@ -1,8 +1,12 @@
 import time
 import script
 from colorama import Fore
+from art import text2art
 
 def main():
+    print(text2art("W E L C O M E !"))
+    print(text2art("S E R V E R !"))
+    
     now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
     print(now + "\n")
@@ -10,6 +14,7 @@ def main():
     question = input("Do you want to start ? (y/n) :")
 
     if question == "y" or question == "Y":
+        print(text2art("S T A R T !")
         print("Okay!")
         script.zz()
     else:
