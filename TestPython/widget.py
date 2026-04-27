3819909719f9b5725754dd5494c7521862524ca8
SHALAWA <bambukvova3@gmail.com>
2026-04-26 23:41:26 -0400

    Test App (With Bugs)

---
+++ TestPython/widget.py
@@ -0,0 +1,30 @@
+# This Python file uses the following encoding: utf-8
+import sys
+
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_form import Ui_Widget
+
+class Widget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Widget()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = Widget()
+    
+    label = QLabel("Welcome!")
+    label.show()
+    
+    butt = QPushButton("Leave")
+    butt.show()
+    
+    widget.show()
+    sys.exit(app.exec())
