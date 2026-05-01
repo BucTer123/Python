4ec6f4ef4edffc2148cc1db15c4c09e27005c959
SHALAWA <bambukvova3@gmail.com>
2026-05-01 03:33:20 -0400

    Add files via upload

---
+++ Second Serv DJango/main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from .models import Product
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("main.html")
+        else:
+            return render(request, "login.html", {"error": "Invalid login"})
+
+    return render(request, "login.html")
