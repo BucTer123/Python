5e678aa98231a96aa286d27b70acc93b257e3640
SHALAWA <bambukvova3@gmail.com>
2026-07-20 11:49:31 +0300

    Update script.py

--- WF/Web_Flask/script.py
+++ WF/Web_Flask/script.py
@@ -3,7 +3,23 @@ from flask import Flask, render_template
 serv = Flask(__name__)
 
 @serv.route("/main")
-def main():
+def open_html1():
+    return render_template("main.html")
+
+@serv.route("/")
+def open_html2():
+    return render_template("main.html")
+
+@serv.route("//")
+def open_html4();
+    return render_template("main.html")
+
+@serv.route("/qwerty")
+def open_html5();
+    return render_template("main.html")
+
+@serv,route("/asd")
+def open_html6():
     return render_template("main.html")
 
 def zz():
